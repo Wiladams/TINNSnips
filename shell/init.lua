6fdc4e3f8b65304bde3e50da08bf321fd2b7e635
William Adams <william_a_adams@msn.com>
2013-04-29 10:55:45 -0700

    new file:   init.lua

---
+++ shell/init.lua
@@ -0,0 +1,20 @@
+-- shell.lua
+local langutils = require("langutils");
+
+import = function(name)
+--print("Importing: ", name);
+	local module = require(name);
+--print("Module: ", module);
+
+	if module then
+		langutils.makeGlobal(module);
+	end
+end
+
+exit = function(retValue)
+	assert(false, retValue);
+end
+
+import 'shell.ver';
+import 'datetime';
+import 'SysInfo';
